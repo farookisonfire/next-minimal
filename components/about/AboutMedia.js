8d7f66f0149de311f48648fd25b43690812ec386
Farook Khan <farook.khan@gmail.com>
2017-09-03 16:26:53 -0700

    Add photos to learn more page.

--- components/about/AboutMedia.js
+++ components/about/AboutMedia.js
@@ -11,34 +11,34 @@ const AboutMedia = () => (
       <p>Mentorship</p>
       <Grid.Row columns={3}>
         <Grid.Column>
-          <Image src='https://farm6.staticflickr.com/5682/20926008853_e44d5eae81.jpg' />
-        </Grid.Column>
-        <Grid.Column>
-          <Image src='https://farm1.staticflickr.com/739/20924115604_04d25e0a5a_k.jpg' />
+          <Image src='https://s3.amazonaws.com/minimal-spaces/mentorship.jpg' />
         </Grid.Column>
         <Grid.Column>
           <Image src='https://farm6.staticflickr.com/5794/21358787200_2fd2fba0e6.jpg' />
+        </Grid.Column>
+        <Grid.Column>
+          <Image src='https://s3.amazonaws.com/minimal-spaces/mentorship2.jpg' />
         </Grid.Column>
       </Grid.Row>
       <p>Health Innovation</p>
       <Grid.Row columns={3}>
         <Grid.Column>
-          <Image src='https://farm6.staticflickr.com/5682/20926008853_e44d5eae81.jpg' />
+          <Image src='https://s3.amazonaws.com/minimal-spaces/health2.jpg' />
         </Grid.Column>
         <Grid.Column>
           <Image src='https://farm6.staticflickr.com/5718/21358224520_64f92a1331.jpg' />
         </Grid.Column>
         <Grid.Column>
-          <Image src='https://farm6.staticflickr.com/5794/21358787200_2fd2fba0e6.jpg' />
+          <Image src='https://s3.amazonaws.com/minimal-spaces/health3.jpg' />
         </Grid.Column>
       </Grid.Row>
       <p>Cultural Immersion</p>
       <Grid.Row columns={3}>
         <Grid.Column>
-          <Image src='https://farm6.staticflickr.com/5682/20926008853_e44d5eae81.jpg' />
+          <Image src='https://s3.amazonaws.com/minimal-spaces/culture2.jpg' />
         </Grid.Column>
         <Grid.Column>
-          <Image src='https://farm1.staticflickr.com/739/20924115604_04d25e0a5a_k.jpg' />
+          <Image src='https://s3.amazonaws.com/minimal-spaces/culture3.jpg' />
         </Grid.Column>
         <Grid.Column>
           <Image src='https://farm6.staticflickr.com/5794/21358787200_2fd2fba0e6.jpg' />
@@ -47,13 +47,13 @@ const AboutMedia = () => (
       <p>Cape Town</p>
       <Grid.Row columns={3}>
         <Grid.Column>
-          <Image src='https://farm6.staticflickr.com/5682/20926008853_e44d5eae81.jpg' />
+          <Image src='https://s3.amazonaws.com/minimal-spaces/capetown.jpg' />
         </Grid.Column>
         <Grid.Column>
-          <Image src='https://farm1.staticflickr.com/739/20924115604_04d25e0a5a_k.jpg' />
+          <Image src='https://s3.amazonaws.com/minimal-spaces/capetown2.jpg' />
         </Grid.Column>
         <Grid.Column>
-          <Image src='https://farm6.staticflickr.com/5794/21358787200_2fd2fba0e6.jpg' />
+          <Image src='https://s3.amazonaws.com/minimal-spaces/capetown1.jpg' />
         </Grid.Column>
       </Grid.Row>
     </Grid>
